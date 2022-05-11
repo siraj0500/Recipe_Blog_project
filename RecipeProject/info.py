3e42832615f78b575ecddf24ada3ca16fd51f1c4
Sirajudheeen <101247065+siraj0500@users.noreply.github.com>
2022-05-11 19:21:56 +0530

    Update info.py

--- RecipeProject/info.py
+++ RecipeProject/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'godsownrecipe@gmail.com'
-EMAIL_HOST_PASSWORD = 'Own@_050'
+EMAIL_HOST_USER = 'your email @gmail.com'
+EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
